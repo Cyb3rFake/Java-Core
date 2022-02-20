928c0731f1b21d04a1c180ad324f4c2e78c5aa18
aruytehno <aruytehno@gmail.com>
2022-02-21 02:08:01 +0300

    Added structure

---
+++ src/main/java/com/skillbox/Main.java
@@ -0,0 +1,27 @@
+package com.skillbox;
+
+public class Main {
+    public static void main(String[] args) {
+        write("Hello World!");
+        write("Hello World!", 10);
+
+    }
+
+    /**
+     * Напечатать текст
+     * @param text
+     */
+    public static void write(String text) {
+        System.out.printf("%s\n", text);
+    }
+
+    /**
+     * Напечатать текст n-раз
+     * @param text
+     * @param repeat
+     */
+    public static void write(String text, int repeat) {
+        for (int i = 0; i < repeat; i++)
+            System.out.printf("%s %s\n", text, i + 1);
+    }
+}
---
+++ src/test/java/com/skillbox/MainTest.java
@@ -0,0 +1,19 @@
+package com.skillbox;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void write() {
+    }
+
+    @Test
+    void testWrite() {
+        Main main = new Main();
+        main.write("Hello World!");
+        main.write("Hello World!", 10);
+    }
+}
