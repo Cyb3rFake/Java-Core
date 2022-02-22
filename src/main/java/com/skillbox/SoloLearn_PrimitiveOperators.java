a975a63c58680804c5137fc7f81b039c2cbe1517
korvyakov_fn <enemy1987@yandex.ru>
2022-02-22 11:10:34 +0300

    2 практических задачи по операторам

---
+++ src/main/java/com/skillbox/SoloLearn_PrimitiveOperators.java
@@ -0,0 +1,55 @@
+package com.skillbox;
+//Primitive Operators
+
+//        Task
+//        Complete the code to output the distance in kilometers and meters each on a new line.
+//
+//        Sample Input
+//        3644
+//
+//        Sample Output
+//        3
+//        644
+//
+//        Explanation
+//        1 kilometer is 1000 meters, therefore 3644 meters is equal to 3 kilometers and 644 meters.
+import java.util.Scanner;
+
+public class SoloLearn_PrimitiveOperators {
+//    public static void main(String[] args) {
+//        Scanner read = new Scanner("3644");
+//        int distance = read.nextInt();
+//        System.out.print(distance / 1000 + "\n" +distance % 1000 );
+//    }
+
+
+
+    /**
+     Students are given homework in math, history, and geometry.
+     Write a program that takes the time spent on each subject as input, and calculates and outputs the total number of hours and minutes spent on each subject, each on a new line accordingly.
+
+     Sample Input
+     35
+     40
+     39
+
+     Sample Output
+     1
+     54
+
+     Explanation
+     The total amount of spent minutes is 114, which is equal to 1 hour (the first output) and 54 minutes (the second output).
+
+
+    */
+
+//    public static void main(String[] args) {
+//        int math = 35;
+//        int history = 40;
+//        int geometry = 39;
+//        int sum = math + history + geometry;
+//        System.out.print(sum/60+"\n"+sum%60);
+//    }
+
+
+}
--- src/test/java/com/skillbox/MainTest.java
+++ src/test/java/com/skillbox/MainTest.java
@@ -6,14 +6,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
+
+
+
+
+
     @Test
     void write() {
     }
 
-    @Test
-    void testWrite() {
-        Main main = new Main();
-        main.write("Hello World!");
-        main.write("Hello World!", 10);
-    }
+//    @Test
+//    void testWrite() {
+//        Main main = new Main();
+//        main.write("Hello World!");
+//        main.write("Hello World!", 10);
+//    }
+
 }
