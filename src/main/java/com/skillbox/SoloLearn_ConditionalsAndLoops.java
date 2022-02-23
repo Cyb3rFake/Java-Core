c3a1c8ff4e04b6cdc3286152c7db49ca11fa530a
Cyb3rFake <nevergreen17@gmail.com>
2022-02-24 00:22:42 +0300

    Условные операторы и циклы 12.3 урок SoloLearn

---
+++ src/main/java/com/skillbox/SoloLearn_ConditionalsAndLoops.java
@@ -0,0 +1,122 @@
+package com.skillbox;
+import java.util.Scanner;
+public class SoloLearn_ConditionalsAndLoops {
+    public static void main(String[] args){
+       /**
+        You have $12,000 to buy a car.
+        You're given a program which takes the price of car as an input.
+
+        Task
+        Output "yes" if the price is lower than or equal to 12,000.
+
+        Sample Input
+        11000
+
+        Sample Output
+        yes
+        */
+//        Scanner scanner = new Scanner(System.in);
+//        int price = scanner.nextInt();
+//        if(price <= 12000) {
+//            System.out.println("yes");
+
+        /**
+         Students can apply for a scholarship if their average grade is higher than 3.5.
+         Write a program that prints “Congratulations” if the student meets the average grade requirement, and “Sorry” if not.
+
+         Sample Input:
+         2.9
+
+         Sample Output:
+         Sorry
+         */
+//        Scanner scanner = new Scanner(System.in);
+//        double score = scanner.nextDouble();
+//        // your code goes here
+//        if (score > 3.5) {
+//            System.out.print("Congratulations");
+//        }
+//        else {
+//            System.out.print("Sorry");
+//        }
+
+
+        /**
+         * The sales manager decided to give a gift card to the customers whose purchases total more than 15000.
+         * On top of this, the customers whose total purchase is above 30000 will receive a second gift card.
+         * You are given a program, which takes the purchase amount as input, and print "Gift card" if it is above 15000.
+         */
+
+//        Scanner read = new Scanner(System.in);
+//        int purchases = read.nextInt();
+//
+//        if(purchases > 15000){
+//            System.out.println("Gift card");
+//            //complete the code
+//
+//            if (purchases > 30000){
+//                System.out.println("Gift card");
+//            }
+//        }
+
+
+        /**
+         * You are an administrator at a football club who must categorize already played games on the team's website.
+         * The given program takes 3 inputs:
+         * 1. match status - which checks if the match is suspended ("true") or not suspended ("false")
+         * 2. your team's score
+         * 3. opposing team's score.
+         * Complete the program so that if the match is suspended (the 1st input is "true"), it will output "Suspended".
+         * If the match is not suspended ( the1st output is false), the following statuses should be set depending on the match result: "Won", "Lost" and "Draw".
+         *
+         * Sample Input
+         * false
+         * 3
+         * 2
+         *
+         * Sample Output
+         * Won
+         */
+//        Scanner read = new Scanner(System.in);
+//        boolean isSuspended = read.nextBoolean();
+//        int ourScore = read.nextInt();
+//        int theirScore = read.nextInt();
+//        // your code goes here
+//        if (isSuspended == false){
+//            if (ourScore > theirScore){
+//                System.out.print("Won");
+//            }
+//            if (ourScore == theirScore){
+//                System.out.print("Draw");
+//            }
+//            if (ourScore < theirScore){
+//                System.out.print("Lost");
+//            }
+//        }
+//        else{
+//            System.out.print("Suspended");
+//        }
+//    }
+
+
+        /**
+         *
+         */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
+
