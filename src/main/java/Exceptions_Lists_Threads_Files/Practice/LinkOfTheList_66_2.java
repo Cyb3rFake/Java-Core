ce1567b1490992fc9cf4c0fa028b6bf8593a4994
Cyb3rFake <nevergreen17@gmail.com>
2022-03-09 22:22:43 +0300

    Решено практическое задание 66.2

--- src/main/java/Exceptions_Lists_Threads_Files/Practice/LinkOfTheList_66_2.java
+++ src/main/java/Exceptions_Lists_Threads_Files/Practice/LinkOfTheList_66_2.java
@@ -1,4 +1,26 @@
 package Exceptions_Lists_Threads_Files.Practice;
 
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Iterator;
+
 public class LinkOfTheList_66_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        LinkedList<Integer> nums = new LinkedList<Integer>();
+
+        while (nums.size()<5){
+            int num =scanner.nextInt();
+            nums.add(num);
+        }
+
+        int sum = 0;                                    //объявляем счетчик эелементов
+        Iterator<Integer> n = nums.iterator();          //создаем экземпляр метода Iterator n с типом Integer для списка nums
+        while (n.hasNext()){                            //создаем цикл while на количество итераций равное числу элементов списка
+            Integer value = n.next();                   //создаем переменнут value типа Integer в нутри цикла в которую передам значение списка на каждой интерации
+            sum +=value;                                //складываем в переменную sum значение value на каждой инетрации
+        }
+        System.out.println(sum);
+    }
 }
--- src/main/java/Exceptions_Lists_Threads_Files/Practice/MinimumAndMaximum_65_2.java
+++ src/main/java/Exceptions_Lists_Threads_Files/Practice/MinimumAndMaximum_65_2.java
@@ -1,5 +1,20 @@
 package Exceptions_Lists_Threads_Files.Practice;
 
-public class MinimumAndMaximum_65_2 {
+import java.util.ArrayList;
+import java.util.Collections; //добавляем метод Collections
+import java.util.Scanner;
 
+public class MinimumAndMaximum_65_2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (nums.size()<5){
+            int num = scanner.nextInt();
+            nums.add(num);                          //добваляем введеное число в список
+        }
+       System.out.println(Collections.max(nums));   //выводим максимальное число списка
+       System.out.println(Collections.min(nums));   //выводим минимальное число списка
+    }
 }
